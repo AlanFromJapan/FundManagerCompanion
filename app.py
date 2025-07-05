c216ef5de4bd37474dc4fb0eec38887a3d01a8dd
AlanFromJapan <alanfromjapan@gmail.com>
2025-07-05 14:54:51 +0900

    Add Basic view of funds list

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template
+import sqlite3
+from fund import Fund
+
+DB_PATH = 'data/data.db'
+app = Flask(__name__)
+
+@app.route('/')
+def show_funds():
+    funds = get_all_funds()
+    return render_template('funds.html', funds=funds)
+
+def get_all_funds():
+    conn = sqlite3.connect(DB_PATH)
+    cur = conn.cursor()
+    
+    cur.execute('SELECT FundID, Name, Currency FROM FUND')
+    rows = cur.fetchall()
+
+    #ugly but not so many funs that it counts
+    cur.execute("SELECT * FROM FUND_CODE")
+    codes = cur.fetchall()
+
+    conn.close()
+
+    funds = [Fund.from_db_row(row) for row in rows]
+
+    for fund in funds:
+        for code in codes:
+            if fund.fund_id == code[0]:
+                fund.codes[code[1]] = code[2]  # Assuming code[1] is the system and code[2] is the value
+
+
+    return funds
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ fund.py
@@ -0,0 +1,14 @@
+class Fund:
+    def __init__(self, fund_id, name, currency):
+        self.fund_id = fund_id
+        self.name = name
+        self.currency = currency
+        self.codes = {}
+
+    @classmethod
+    def from_db_row(cls, row):
+        # Assumes row is (FundID, Name, Currency)
+        return cls(row[0], row[1], row[2])
+
+    def __repr__(self):
+        return f"<Fund id={self.fund_id} name={self.name} currency={self.currency}>"
