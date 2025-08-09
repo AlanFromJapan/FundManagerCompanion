3984cba3d5db7ef5b9eec4c9765ab30eaae78d7d
AlanFromJapan <alanfromjapan@gmail.com>
2025-08-09 23:24:46 +0900

    Show latest nav,  get 300 but show 100 only (need 300 for stats coming next)

--- bp_fund_detail/fund_detail.py
+++ bp_fund_detail/fund_detail.py
@@ -7,7 +7,8 @@ bp_fund_details = Blueprint('bp_fund_details', __name__)
 
 @bp_fund_details.route('/funds/<int:fund_id>', methods=['GET','POST'])
 def show_fund_page(fund_id):
-    MAX_NAV_LIMIT = 100  # Limit for NAV records to fetch
+    MAX_NAV_LIMIT = 300  # Limit for NAV records to fetch
+    MAX_NAV_SHOWN = 100  # Limit for NAV records to show in the chart
 
     funds = get_all_funds()
     fund = next((f for f in funds if f.fund_id == fund_id), None)
@@ -30,11 +31,11 @@ def show_fund_page(fund_id):
     if fund.nav:
         #reverse the nav_sorted to have oldest first (L to R)
         #show the last 100 NAVs
-        snav = fund.nav_sorted
+        snav = fund.nav_sorted[:MAX_NAV_SHOWN]
         snav.reverse()  # Reverse to have oldest first
 
         values = [{'x': date, 'y': int(nav)} for date, nav in snav]
         labels = [date[:10] for date, _ in snav]
 
 
-    return render_template('fund_detail.html', fund=fund, chartData=(values, labels, MAX_NAV_LIMIT))
+    return render_template('fund_detail.html', fund=fund, chartData=(values, labels, MAX_NAV_LIMIT, MAX_NAV_SHOWN))
