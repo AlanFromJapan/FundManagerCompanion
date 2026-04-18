3598b8c4e3d13ae3f40dbaaf21d98b24c22afae2
AlanFromJapan <alanfromjapan@gmail.com>
2026-04-18 22:51:11 +0900

    Feat added Monex transaction file import (buy only for tonight)

--- bp_transactions/transactions.py
+++ bp_transactions/transactions.py
@@ -31,20 +31,80 @@ def register_transaction():
         flash('All fields are required.', 'error')
         return redirect(url_for('bp_transactions.register_transaction_form'))
     try:
-        conn = sqlite3.connect(conf['DB_PATH'])
-        cur = conn.cursor()
-        cur.execute('INSERT INTO XACT (TradeDate, ExecutionDate, XactType, FundID, Unit, XactPrice, UnitPrice) VALUES (?, ?, ?, ?, ?, ?, ?)',
-                    (reception_date, reception_date, trans_type, fund_id, quantity, amount, unitprice))
-        conn.commit()
-        conn.close()  
-        flash('Transaction registered successfully.', 'success')
-
-        # Recalculate positions since the transaction date -1d in case
-        start_date = datetime.datetime.strptime(reception_date, '%Y-%m-%d') - datetime.timedelta(days=1)
-        recalculate_positions(start_date=start_date, fund_id=int(fund_id))
+        start_date = xact_save_to_db(fund_id, trans_type, reception_date, quantity, amount, unitprice)
 
         flash(f'Positions recalculated successfully since {start_date}.', 'success')
 
     except Exception as e:
         flash(f'Error registering transaction: {e}', 'error')
     return redirect(url_for('bp_transactions.transactions_page'))
+
+
+
+def xact_save_to_db(fund_id, trans_type, reception_date, quantity, amount, unitprice):
+    conn = sqlite3.connect(conf['DB_PATH'])
+    cur = conn.cursor()
+    cur.execute('INSERT INTO XACT (TradeDate, ExecutionDate, XactType, FundID, Unit, XactPrice, UnitPrice) VALUES (?, ?, ?, ?, ?, ?, ?)',
+                    (reception_date, reception_date, trans_type, fund_id, quantity, amount, unitprice))
+    conn.commit()
+    conn.close()  
+    flash('Transaction registered successfully.', 'success')
+
+        # Recalculate positions since the transaction date -1d in case
+    start_date = datetime.datetime.strptime(reception_date, '%Y-%m-%d') - datetime.timedelta(days=1)
+    recalculate_positions(start_date=start_date, fund_id=int(fund_id))
+    return start_date
+
+
+
+@bp_transactions.route('/transactions/csvimportMonex', methods=['POST'])
+def import_transactions_from_csv_monex():   
+    try:
+        file = request.files.get('csv_file')
+        if not file:
+            flash('No file uploaded.', 'error')
+            return redirect(url_for('bp_transactions.transactions_page'))
+        
+        # Process the CSV file and import transactions
+        import csv
+        s = file.stream.read().decode('shift_jis')  # Assuming the CSV is in Shift JIS encoding 
+        reader = csv.DictReader(s.splitlines())
+        for row in reader:
+            fund_name = row.get('銘柄名')
+            csv_trans_type = row.get('取引区分')
+
+            if csv_trans_type == '買付（累投）':
+                csv_reception_date = str(row.get('受渡日')).replace('/', '-')  # Convert date format if needed to expected "YYYY-MM-DD"
+                csv_quantity = int(row.get('数量'))
+                csv_amount = int(str(row.get('受渡金額')).replace('-', ''))  # Remove commas from the amount
+                csv_price = int(row.get('約定価格'))
+
+                #purchase transaction, insert into DB
+                print(f"Found purchase transaction in CSV for fund {fund_name} on {csv_reception_date} with quantity {csv_quantity} and amount {csv_amount}")
+
+                # Get fund ID from name
+                # The Monex file uses the full name of the fund and not code, so we need to find the fund ID based on the full name. This is not optimal but we don't have the full name in the DB, only the code, so we need to do this transcoding here. 
+                funds = get_all_funds()
+                fund_id = None
+                for f in funds:
+                    if "monex_fullname"in f.codes and f.codes["monex_fullname"] == fund_name:
+                        fund_id = f.fund_id
+                        break
+                if not fund_id:
+                    #missing transcoding: aborting
+                    flash(f'Fund "{fund_name}" not found in the system. Failed importing this transactions, moving to next.', 'error')
+                    continue
+
+                # Save transaction to DB
+                xact_save_to_db(fund_id, 'お買付', csv_reception_date, csv_quantity, csv_amount, csv_price)
+                print(f"Transaction for fund {fund_name} of amount {csv_amount} imported successfully.")
+
+
+
+
+        flash('Transactions imported successfully from CSV.', 'success')
+    except Exception as e:
+        flash(f'Error importing transactions from CSV: {e}', 'error')
+        print(e)
+
+    return redirect(url_for('bp_transactions.transactions_page')) 
